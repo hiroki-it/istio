075a5822809827a614d386e8fc77aded32a17546
hiroki-it <hasegawafeedshop@gmail.com>
2025-03-21 04:24:38 +0900

    istio-demo用のスクリプトを作成した

--- samples/bookinfo/src/build-service-istio-demo.sh
+++ samples/bookinfo/src/build-service-istio-demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-BOOKINFO_HUB=docker.io/hirokihasegawa BOOKINFO_TAG=1.20.2-istio-demo ./build-services.sh
+BOOKINFO_HUB=docker.io/hirokihasegawa BOOKINFO_TAG=1.20.2-istio-demo ./build-services-istio-demo.sh
