bd1aa138ec4171a4775f94ec81f883eb2e8605b7
hiroki-it <hasegawafeedshop@gmail.com>
2025-03-29 07:22:51 +0900

    エラーメッセージを改善した

--- samples/bookinfo/src/productpage/productpage.py
+++ samples/bookinfo/src/productpage/productpage.py
@@ -302,7 +302,7 @@ def front():
 
     # reviewsサービスにリクエストを送信する
     reviewsStatus, reviews = getProductReviews(product_id, headers)
-    logging.info("[" + str(reviewsStatus) + "] details response is " + str(reviews))
+    logging.info("[" + str(reviewsStatus) + "] reviews response is " + str(reviews))
 
     # いずれかのマイクロサービスでアクセストークンの検証が失敗し、401ステータスが返信された場合、ログアウトする
     if detailsStatus == 401 or reviewsStatus == 401:
